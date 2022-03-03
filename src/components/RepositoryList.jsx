a80860a3fcfe7a76b3e3bd2cc3b01d7c1ccb3dba
Jorge Eder <jorgeeder.js@gmail.com>
2022-03-03 15:11:56 -0300

    Listando repositorios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,14 +2,6 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
-// https://api.github.com/orgs/rocketseat/repos
-// https://api.github.com/users/jorgeeder/repos
-
-const repository = {
-    name: 'unform',
-    description:"Forms in React",
-    link:"http://github.com/unform/unform",
-}
 
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
@@ -25,10 +17,10 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository= {repository}/>
-                <RepositoryItem repository= {repository}/>
-                <RepositoryItem repository= {repository}/>
-                <RepositoryItem repository= {repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository= {repository}/>
+                })}
+             
             </ul>
         </section>
     );
